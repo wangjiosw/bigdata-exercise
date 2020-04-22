ea5091a6eec89782c83f34f8800be1681cf0ca40
wangjiosw <58982170+wmr404@users.noreply.github.com>
2020-04-22 15:47:45 +0800

    flume+kafka+storm

--- wordcount/src/com/sxt/test/test.java
+++ wordcount/src/com/sxt/test/test.java
@@ -6,6 +6,9 @@ import com.sxt.storm.spout.WcSpout;
 
 import backtype.storm.Config;
 import backtype.storm.LocalCluster;
+import backtype.storm.StormSubmitter;
+import backtype.storm.generated.AlreadyAliveException;
+import backtype.storm.generated.InvalidTopologyException;
 import backtype.storm.topology.TopologyBuilder;
 import backtype.storm.tuple.Fields;
 
@@ -16,12 +19,21 @@ public class test {
 
 		TopologyBuilder tb = new TopologyBuilder();
 		tb.setSpout("wcspout", new WcSpout());
-		tb.setBolt("wsplitbolt", new WsplitBolt(),3).shuffleGrouping("wcspout");
-		tb.setBolt("wcbolt", new WcBolt(),3).fieldsGrouping("wsplitbolt", new Fields("w"));
-		
-		// 放入本地集群
-		LocalCluster lc = new LocalCluster();
-		lc.submitTopology("wc", new Config(), tb.createTopology());
+		tb.setBolt("wsplitbolt", new WsplitBolt()).shuffleGrouping("wcspout");
+		tb.setBolt("wcbolt", new WcBolt()).fieldsGrouping("wsplitbolt", new Fields("w"));
+		 Config config = new Config();
+		if (args.length > 0) {
+			try {
+				StormSubmitter.submitTopology(args[0], config, tb.createTopology());
+			} catch (AlreadyAliveException | InvalidTopologyException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}else {
+			// 放入本地集群
+			LocalCluster lc = new LocalCluster();
+			lc.submitTopology("wc", config, tb.createTopology());			
+		}
 
 	}
 
